3ffe91d77a048b1fc726e29c00e43709723621bc
Chans2222 <34190277+Chans2222@users.noreply.github.com>
2018-08-22 15:29:55 +0900

    ...

--- LegLeg.sol
+++ LegLeg.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.4.23;
 contract LegLeg {
 
     address Owner = 0x50D487BE7017815BC6aAe1Be4D6F8f62b21c9230;
+    uint randNum;
 
     event Failed(address beneficiary, uint amount);
     event Successed(address beneficiary, uint amount);
@@ -21,17 +22,18 @@ contract LegLeg {
         _;
     }
 
-    function game1() public payable {
-        if (uint(keccak256(abi.encodePacked(now, msg.sender, msg.value))) % 2 == 1) {
-            msg.sender.transfer(msg.value * 2);
+    function game1(uint inputNum) public payable {
+        randNum = uint(keccak256(abi.encodePacked(now, msg.sender, msg.value))) % 100;
+        if (inputNum >= randNum) {
+            msg.sender.transfer(msg.value * 98.5/inputNum);
         }
     }
 
-     function game2() public payable {
+/*     function game2() public payable {
         if (uint(keccak256(abi.encodePacked(now, msg.sender, msg.value))) % 5 == 1) {
             msg.sender.transfer(msg.value * 5);
         }
-    }
+    }*/
 
      function choice(uint choiceNum) public payable spare returns(uint) {
         if (choiceNum == 1) {
